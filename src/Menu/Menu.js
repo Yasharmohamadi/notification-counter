6a3a4de5ce443fac503934b776789da31b9820b0
Yashar <yashar22mo@gmail.com>
2024-06-22 23:35:18 +0330

    Add 2 way for solution

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -2,48 +2,63 @@ import "./Menu.css";
 import React from "react";
 
 export default class Menu extends React.Component {
-    constructor() {
-        super()
+	constructor() {
+		super();
 
-        this.state = {
-            nofifications: [
-                'New',
-                'New',
-                'New',
-                'New',
-                'New',
-                'New',
-                'New',
-                'New'
-            ]
-        }
+		this.state = {
+			nofifications: [
+				"New",
+				"New",
+				"New",
+				"New",
+				"New",
+				"New",
+				"New",
+				"New",
+				"New",
+				"New",
+			],
+		};
+	}
 
-    }
+	render() {
+		// if (this.state.nofifications.length === 0) {
+		// 	console.log("خبری نیست");
+		// 	return (
+		// 		<div className="container">
+		// 			<h1 className="title">React</h1>
+		// 			<div className="news">
+		// 				<h4 className="news__text">News</h4>
+		// 			</div>
+		// 		</div>
+		// 	);
+		// } else {
+		// 	console.log("خبر داریم!");
+		// 	return (
+		// 		<div className="container">
+		// 			<h1 className="title">React</h1>
+		// 			<div className="news">
+		// 				<h4 className="news__text">News</h4>
+		// 				<p className="news__number">{this.state.nofifications.length}</p>
+		// 			</div>
+		// 		</div>
+		// 	);
+		// }
+		//////////////////////////////////////////////////////////////////////////////////
+		let newsCount = null
+		if (this.state.nofifications.length !== 0) {
+		    newsCount = <p className="news__number">{this.state.nofifications.length}</p>
+		}
 
-    render() {
-        if (this.state.nofifications.length === 0) {
-            console.log('خبری نیست');
-            return (
-                <div className="container">
-                    <h1 className="title">React</h1>
-                    <div className="news">
-                        <h4 className="news__text">News</h4>
-                    </div>
-                </div>
-            );
-        } else {
-            console.log('خبر داریم!');
-            return (
-                <div className="container">
-                    <h1 className="title">React</h1>
-                    <div className="news">
-                        <h4 className="news__text">News</h4>
-                        <p className="news__number">{this.state.nofifications.length}</p>
-                    </div>
-                </div>
-            );
-            
-        }
+		return (
+		    <div className="container">
+		        <h1 className="title">React</h1>
+		        <div className="news">
+		            <h4 className="news__text">News</h4>
+		            {newsCount}
+		        </div>
+		    </div>
+		);
 
-    }
+	}
 }
