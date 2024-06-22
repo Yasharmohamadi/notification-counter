e3cc81fa15068138c4c04d62f10df3e6c198bac9
Yashar <yashar22mo@gmail.com>
2024-06-22 23:12:20 +0330

    Add menu.js codes

---
+++ src/Menu/Menu.js
@@ -0,0 +1,13 @@
+import "./Menu.css";
+
+export default function Menu() {
+	return (
+		<div className="container">
+			<h1 className="title">React</h1>
+			<div className="news">
+				<h4 className="news__text">News</h4>
+				<p className="news__number">123</p>
+			</div>
+		</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Menu from './Menu/Menu'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
+        <Menu />
     </div>
 );
 
