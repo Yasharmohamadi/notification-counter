9aa68fcb3c3c8e00c005cbc0dc2dc8120fea85eb
Yashar <yashar22mo@gmail.com>
2024-06-22 23:30:01 +0330

    Add first way for solution

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,13 +1,49 @@
 import "./Menu.css";
+import React from "react";
 
-export default function Menu() {
-	return (
-		<div className="container">
-			<h1 className="title">React</h1>
-			<div className="news">
-				<h4 className="news__text">News</h4>
-				<p className="news__number">123</p>
-			</div>
-		</div>
-	);
+export default class Menu extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            nofifications: [
+                'New',
+                'New',
+                'New',
+                'New',
+                'New',
+                'New',
+                'New',
+                'New'
+            ]
+        }
+
+    }
+
+    render() {
+        if (this.state.nofifications.length === 0) {
+            console.log('خبری نیست');
+            return (
+                <div className="container">
+                    <h1 className="title">React</h1>
+                    <div className="news">
+                        <h4 className="news__text">News</h4>
+                    </div>
+                </div>
+            );
+        } else {
+            console.log('خبر داریم!');
+            return (
+                <div className="container">
+                    <h1 className="title">React</h1>
+                    <div className="news">
+                        <h4 className="news__text">News</h4>
+                        <p className="news__number">{this.state.nofifications.length}</p>
+                    </div>
+                </div>
+            );
+            
+        }
+
+    }
 }
